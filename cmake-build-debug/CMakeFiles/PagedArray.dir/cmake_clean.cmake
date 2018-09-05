64bef4b43a8c9b5380d7dc26cbe8bb52b91afe4a
Andrey Sibaja <diego.garro120@gmail.com>
2018-09-04 22:51:10 -0600

    Commit

---
+++ cmake-build-debug/CMakeFiles/PagedArray.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PagedArray.dir/main.cpp.o"
+  "PagedArray.pdb"
+  "PagedArray"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PagedArray.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
