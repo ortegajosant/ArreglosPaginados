57bd2c1e6693733704791c8c45442b8878ccc4bb
Andrey Sibaja <diego.garro120@gmail.com>
2018-09-04 23:25:06 -0600

    Clase Page

---
+++ Page/Page.cpp
@@ -0,0 +1,5 @@
+//
+// Created by andrew on 04/09/18.
+//
+
+#include "Page.h"
---
+++ Page/Page.h
@@ -0,0 +1,14 @@
+//
+// Created by andrew on 04/09/18.
+//
+
+#ifndef PAGEDARRAY_PAGE_H
+#define PAGEDARRAY_PAGE_H
+
+
+class Page {
+
+};
+
+
+#endif //PAGEDARRAY_PAGE_H
